a89258c6ee2948958535d0359066e1c741e39e1e
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-04-28 22:08:07 +0900

    Update 15655_N과 M(6).cpp

--- BackTracking/15655_N과 M(6).cpp
+++ BackTracking/15655_N과 M(6).cpp
@@ -1,3 +1,11 @@
+//N개의 자연수와 자연수 M이 주어졌을 때,
+//아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램
+//N개의 자연수는 모두 다른 수
+//고른 수열은 오름차순이어야 함
+//수열은 사전 순으로 증가하는 순서로 출력해야 함
+// 중복되는 수열을 여러 번 출력하면 안됨
+// 각 수열은 공백을 구분해서 출력
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,7 +19,7 @@ bool isused[10001];
 void hello(vector <int> vt) {
 	
 	for (int i = 0; i < vt.size(); i++) {
-		isused[vt[i]] = false; //isused[2] = 0, isused[4] = 0, isused[5] = 0
+		isused[vt[i]] = false;
 	}
 	
 }
@@ -49,10 +57,10 @@ int main() {
 	vector <int> v(n);
 
 	for (int i = 0; i < n; i++) {
-		cin >> v[i]; // 4 5 2 
+		cin >> v[i]; 
 	}
 
-	sort(v.begin(), v.end()); //v[0] = 2, v[1] = 4, v[2] = 5
+	sort(v.begin(), v.end()); 
 
 	end_value = v[n - 1];
 
